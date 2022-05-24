89ba53d41028ad575501c9733eb30209836194bf
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-24 15:28:30 -0700

    change textfield

--- snacks/models.py
+++ snacks/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth import get_user_model
 class Snack(models.Model):
     
     name = models.CharField(max_length=64)
-    description = models.CharField(max_length=250)
+    description = models.TextField(max_length=250)
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     

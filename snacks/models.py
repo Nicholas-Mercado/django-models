cb2797b33c59a5dc9fd3923a8cf014c5c56b5985
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-24 11:20:36 -0700

    created user model

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Snack(models.Model):
+    
+    name = models.CharField(max_length=256)
+    rating = models.ImageField(default=0)
+    reviewer = models.ForeignKey(get_user_model(), models.CASCADE)
+    
